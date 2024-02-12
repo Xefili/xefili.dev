f031c88db821e4ac236b3f077bbd951cd1908d16
xefili <henrycarl2007@gmail.com>
2024-02-12 09:43:53 +0100

    include /docs subdirectory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"], //narrow down to speed up build times
+  content: ["./*.html", "./docs/*.html"], //narrow down to speed up build times
   theme: {
     fontFamily: {
       'sans': ['Roboto Flex', 'system-ui', 'sans-serif'],

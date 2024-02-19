9882b5b8dc29d08021ef6aa26fbe1ba0d6db394f
xefili <henrycarl2007@gmail.com>
2024-02-19 22:32:46 +0100

    Include JS files in the /blog directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./docs/*.html", "./tools/*.html", "./blog/*.html", "./zeus/*.html"], //narrow down to speed up build times
+  content: ["./*.html", "./docs/*.html", "./tools/*.html", "./blog/*.html", "./blog/*.js", "./zeus/*.html"], //narrow down to speed up build times
   theme: {
     fontFamily: {
       'sans': ['Roboto Flex', 'system-ui', 'sans-serif'],

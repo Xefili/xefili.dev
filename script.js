160fbc4a7881266b6ccc24e0626fd883cf8a671e
xefili <henrycarl2007@gmail.com>
2024-02-25 15:44:47 +0100

    Should now always display newest article at the top

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
 function displayAction(e) {
-    try {
-        document.getElementById("alertBox").innerHTML =
-        `
-        Couldn't fetch posts from api. Check your connection or contact the webpage's administrator.
-        Error: ${e}
-        `;
-        document.getElementById("action").classList.toggle("hidden");
-    } catch (e) {
-        console.error("Couldn't find element tagged alertBox");
-    }
+    alert(`Looks like we've hit a roadblock. If this problem persists, contact mail@xefili.dev with the following error code: ${e}`);
 }
-document.getElementById("close").addEventListener(onclick, () => {
-    document.getElementById("action").classList.toggle("hidden");
-})
 // GET blog contents
 try {
-    const response = await fetch("https://api.xefili.dev/articles/", {method:"GET", "mode":"no-cors"});
-    const arrLen = await response.length;
+    const response = await fetch("https://api.xefili.dev/articles/", {method:"GET"});
+    const jsonReponse = await response.json();
+    var arrLen = jsonReponse.length;
     let i;
-    for(i=arrLen;i>arrLen-4;i--) {
-        const res = response[i];
-        const content = await res.json();
-
-        document.getElementById(`art${i}`).innerHTML = `<h3>${content.title}</h3><text>${content.content}</text>`;
+    let k = 1;
+    for(i=arrLen-1;i>arrLen-5;i--) {
+        console.log(i);
+        document.getElementById(`artTitle${k}`).innerHTML = jsonReponse[i].title;
+        document.getElementById(`artText${k}`).innerHTML = jsonReponse[i].content;
+        document.getElementById(`artLink${k}`).setAttribute("href", `blog/read.html?articleId=${i+1}`);
+        k++;
     }
 } catch(e) {
-    console.error("Failed to connect to server or could not find element");
     displayAction(e);
 }

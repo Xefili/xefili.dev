30ed7678d63de56a0ac425d2ec3da3870be1f407
xefili <henrycarl2007@gmail.com>
2024-09-15 16:54:54 +0200

    Backend and Frontend fixed, look out for bugs

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-function displayAction(e) {
-    alert(`Looks like we've hit a roadblock. If this problem persists, contact mail@xefili.dev with the following error code: ${e}`);
+var data;
+
+async function queryAPI() {
+    data = await fetch("https://api.xefili.dev/articles/", {method:"GET"});
+    data = await data.json();
+    data = data.reverse();
 }
-// GET blog contents
-try {
-    const response = await fetch("https://api.xefili.dev/articles/", {method:"GET", mode:"cors"});
-    const res = await response.json();
-    const jsonReponse = await res.reverse();
-    var arrLen = jsonReponse.length;
-    let i;
-    let k = 1;
-    for(i=0; i!=arrLen; i++) {
-        console.log(i);
-        document.getElementById(`artTitle${k}`).innerHTML = jsonReponse[i].title;
-        document.getElementById(`artText${k}`).innerHTML = jsonReponse[i].content;
-        document.getElementById(`artLink${k}`).setAttribute("href", `blog/read.html?articleId=${i+1}`);
-        k++;
+
+await queryAPI();
+
+let latest = document.getElementById("latest");
+let storybox = document.getElementById("stories");
+
+function fillLatest() {
+    let title = document.createElement("h1");
+    title.innerHTML = data[0].title;
+    title.classList.add("no-underline", "text-3xl");
+    latest.appendChild(title);
+
+    let content = document.createElement("p");
+    content.innerHTML = data[0].content;
+    latest.appendChild(content);
+}
+
+function fillStories() {
+    for(let i = 2; i<=data.length; i++) {
+        let div = document.createElement("div");
+        let title = document.createElement("h1");
+        title.innerHTML = data[i-1].title;
+        title.classList.add("no-underline", "text-3xl");
+        div.appendChild(title);
+        let content = document.createElement("p");
+        content.innerHTML = data[i-1].content;
+        div.appendChild(content);
+        storybox.appendChild(div);
     }
-} catch(e) {
-    displayAction(e);
 }
+
+fillLatest();
+fillStories();

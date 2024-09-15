afcd4e2fdd08135f80f32030f53872dd6b8740b0
xefili <henrycarl2007@gmail.com>
2024-09-15 16:54:26 +0200

    still broken, no idea why

--- blog/getURLParams.js
+++ blog/getURLParams.js
@@ -3,10 +3,11 @@ var id = URLparams.replace("?","").split("=")[1];
 console.info(`Detected article ID ${id}`)
 
 try {
-    var res = await fetch(`https://api.xefili.dev/articles/${id}`, {"method":"GET", "mode":"cors"});
-    var jsonified = await res.json();
-    var articleTitle = jsonified.title;
-    var articleContent = jsonified.content;
+    var res = await fetch(`https://api.xefili.dev/articles`, {"method":"GET", "mode":"cors"});
+    var data = await res.json();
+    var jsonified = await data.reverse();
+    var articleTitle = await jsonified.title;
+    var articleContent = await jsonified.content;
     console.info(`Successfully retrieved article!`);
     document.getElementById("articleTitle").innerHTML = articleTitle;
     document.getElementById("articleContent").innerHTML = articleContent;

6af7415dba865bab2a8e6b64428f63eca0aa8279
Xefili <henrycarl2007@gmail.com>
2025-01-27 18:05:22 +0100

    Fix Front- and Backend

--- script.js
+++ script.js
@@ -20,11 +20,18 @@ function fillLatest() {
     let content = document.createElement("p");
     content.innerHTML = data[0].content;
     latest.appendChild(content);
+    latest.addEventListener("click", () => {
+        window.location.href = `https://xefili.dev/blog/read.html?id=0`;
+    });
 }
 
 function fillStories() {
     for(let i = 2; i<=data.length; i++) {
         let div = document.createElement("div");
+        div.classList.add("border", "p-4", "rounded-2xl");
+        div.addEventListener("click", () => {
+            window.location.href = `https://xefili.dev/blog/read.html?id=${i-1}`;
+        });
         let title = document.createElement("h1");
         title.innerHTML = data[i-1].title;
         title.classList.add("no-underline", "text-3xl");
